36d497e948cb91deda6bd58ba06d54a055eb25d3
Tejas Gupta <tgupta39@wisc.edu>
2025-06-09 15:33:36 +0530

    Minor fixes in Story.js

--- src/components/Story.js
+++ src/components/Story.js
@@ -96,7 +96,7 @@ const Story = () => {
         rowGutter
       );
     });
-    holistic = new Holistic({
+    const holistic = new Holistic({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
       },
@@ -146,8 +146,10 @@ const Story = () => {
     !isAuthenticated || !userName || userName === "USER NOT FOUND" || !userRole;
 
   return (
-    <>
-      {loading && <Loader />}
+  <>
+    {loading ? (
+      <Loader />
+    ) : (
       <Stage
         height={height}
         width={width}
@@ -179,8 +181,10 @@ const Story = () => {
           />
         )}
       </Stage>
-    </>
-  );
+    )}
+  </>
+);
+
 };
 
 export default Story;
