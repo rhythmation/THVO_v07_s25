e1cbb815d0a587f178c275dbf4e0e0abeca7f730
Tejas Gupta <tgupta39@wisc.edu>
2025-08-27 16:33:09 -0500

    Reverting frame buffer logic, will retry implementation
    This reverts commit 24782dbc4c30cd26e0ef5ec765dba0ea57d1c3ff.

--- src/components/ConjecturePoseMatch/ConjecturePoseContainer.js
+++ src/components/ConjecturePoseMatch/ConjecturePoseContainer.js
@@ -1,15 +1,10 @@
+
 import ConjecturePoseMatch from './ConjecturePoseMatch';
 import Background from "../Background";
 import { Graphics } from "@inlet/react-pixi";
 import { darkGray, yellow } from "../../utils/colors";
 import React, { useCallback, useState, useEffect } from "react";
-import { 
-  initializeSession, 
-  bufferPoseDataWithAutoFlush, 
-  startSmartAutoFlush, 
-  stopAutoFlush, 
-  endSession 
-} from "../../firebase/database.js";
+import { promiseChecker, writeToDatabase } from "../../firebase/database.js";
 
 const ConjecturePoseContainer = (props) => {
     const {
@@ -38,76 +33,70 @@ const ConjecturePoseContainer = (props) => {
         const col3 = columnDimensions(3);
         g.drawRect(col3.x, col3.y, col3.width, col3.height);
         g.endFill();
-    }, [columnDimensions]);
+    }, []);
 
     useEffect(() => {
+        // // Defaults recording conditions true and fps = 12.
+    
         const isRecording = "true";
         
         if (isRecording === "true") {
-          // FRAMERATE CAN BE CHANGED HERE
+          // Get the fps parameter: default 12
+
+
+          //FRAMERATE CAN BE CHANGED HERE
           const frameRate = 12;
 
-          let autoFlushId;
-          
-          // Initialize session with static data once
-          const setupSession = async () => {
-            await initializeSession(gameID, frameRate, UUID);
-            
-            // Start auto-flush with hybrid strategy - now passes UUID
-            autoFlushId = startSmartAutoFlush(gameID, UUID, {
-              maxBufferSize: 50,      // Flush when buffer hits 50 frames
-              flushIntervalMs: 2000,  // Or every 2 seconds
-              minBufferSize: 5,       // Don't flush tiny batches
-              frameRate: frameRate    // Pass frameRate for promise checker
-            });
-          };
 
-          // Initialize the session
-          setupSession();
           
-          // Create interval to buffer pose data (much lighter than before)
-          const intervalId = setInterval(() => {
-            // Buffer the pose data - now passes UUID and frameRate
-            bufferPoseDataWithAutoFlush(poseData, gameID, UUID, frameRate);
-          }, 1000 / frameRate);
+          // Empty array to hold promise objects assures that all the promises get settled on component unmount.
+          let promises = [];
     
-          // Cleanup when component unmounts
-          return async () => {
-            // Stop the data collection interval
-            clearInterval(intervalId);
-            
-            // Stop auto-flush
-            if (autoFlushId) {
-              stopAutoFlush(autoFlushId);
-            }
-            
-            // End session - now passes UUID and frameRate
-            await endSession(gameID, UUID, frameRate);
-          };
-        } 
-    }, [gameID, UUID]); // Added UUID to dependencies since it's used in setup
+          // This creates an interval for the writing to the database every n times a second,
+          // where n is a variable framerate.
+          const intervalId = setInterval(() => {
+            // Call the writeToDatabase function with the current poseData, conjecture index,
+            // and fps parameter. Push the resulting promise object to the promises array.
+            promises.push(
+              writeToDatabase(poseData, UUID, frameRate, gameID)
+            );
+            // Call the promiseChecker function to detect any data loss in the promises array
+            // and trigger an alert if necessary.
+            promiseChecker(frameRate, promises);
+        }, 1000 / frameRate);
+    
+          // The code below runs when the component unmounts.
+        return async () => {
+            // Stop the interval when the component unmounts.
+          clearInterval(intervalId);
+    
+            // Wait until all promises are settled so we don't lose data.
+          await Promise.allSettled(promises);
+        };
+      } 
+    }, []);
 
-    // Use background and graphics to draw background and then initiate conjecturePoseMatch
+// Use background and graphics to draw background and then initiate conjecturePoseMatch
     return (
-        <>
-            <Background height={height * 1.1} width={width} />
-            <Graphics draw={drawModalBackground} />
-            <ConjecturePoseMatch
-                poses={poses}
-                tolerances={tolerances}
-                height={height}
-                width={width}
-                columnDimensions={columnDimensions}
-                rowDimensions={rowDimensions}
-                editCallback={editCallback}
-                mainCallback={mainCallback}
-                poseData={poseData}
-                UUID={UUID}
-                onCompleteCallback={onCompleteCallback}
-                needBack={needBack}
-                gameID={gameID}
-            />
-        </>
+    <>
+        <Background height={height * 1.1} width={width} />
+        <Graphics draw={drawModalBackground} />
+        <ConjecturePoseMatch
+            poses={poses}
+            tolerances={tolerances}
+            height={height}
+            width={width}
+            columnDimensions={columnDimensions}
+            rowDimensions={rowDimensions}
+            editCallback={editCallback}
+            mainCallback={mainCallback}
+            poseData={poseData}
+            UUID={UUID}
+            onCompleteCallback={onCompleteCallback}
+            needBack={needBack}
+            gameID={gameID}
+        />
+    </>
     );
 };
 
